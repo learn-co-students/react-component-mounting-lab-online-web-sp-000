40ec70634475f048bbe40463f6ab193a99440c22
kaelaclement <stephanie.kaela@gmail.com>
2019-06-03 19:56:18 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends Component {
 
   //Your code here:
 
-
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
 
 
@@ -55,7 +57,6 @@ class App extends Component {
       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
     }))
   }
-
 
 }
 
