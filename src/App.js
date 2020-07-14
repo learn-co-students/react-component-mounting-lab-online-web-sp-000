ccad889674d2d1e09bef1d93e90e15ae6d824342
betodute <betodute@gmail.com>
2020-07-14 16:10:26 -0700

    complete
    git add .

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Timer from './Timer'
 
 class App extends Component {
 
-  //no props being used here, so we can use the shorthand declaration of state
+  // no props being used here, so we can use the shorthand declaration of state
   state = {
     timerIDs: []
   }
 
 
-  //Your code here:
+  // Your code here:
+
+  componentDidMount() {
+    this.handleAddTimer()
+  }
+
+
+
 
 
 
