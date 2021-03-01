cc450850349e8a1cc9f017202e7fef3493a54d5f
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-03-01 13:51:59 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -7,7 +7,13 @@ class Timer extends Component {
   };
 
   // add your code here
+  componentDidMount(){
+    this.clockTick(1000)
+  }
 
+  componentWillUnmount(){
+    this.stopClock()
+  }
   render() {
     const { time, color } = this.state;
     return (
